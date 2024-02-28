ea306a0055c650ffa13d154da442248aa05b73a0
Favour Ebuka <favourvanderwaal@gmail.com>
2024-02-28 17:53:20 +0100

    auth footer

--- nft-front/src/components/footer/AuthFooter.jsx
+++ nft-front/src/components/footer/AuthFooter.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const AuthFooter = () => {
-  return <div>AuthFooter</div>;
+  return (
+    <div className="auth-footer">
+      <div className="dash-width auth-footer-container">
+        <div>© 2018 - 2023 Axiom, Inc</div>
+        <div>
+          <div className="footer-privacy">Privacy Policy</div>
+          <div></div>
+          <div>Terms of Service</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AuthFooter;
